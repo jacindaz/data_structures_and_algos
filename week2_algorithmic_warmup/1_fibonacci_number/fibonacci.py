f5a1ef4f4d39d43bab48fcd0721226003e8a47ba
Jacinda Zhong <jacindaz@gmail.com>
2018-03-01 14:50:25 -0800

    Python solution for fibonacci

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -5,5 +5,17 @@ def calc_fib(n):
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+def fibonacci(n):
+    results = [0, 1]
+    index = 2
+
+    for x in range(2,n):
+        print(f'\nn: {n}\t\t results[index-1]: {results[index-1]}\t\t results[index-2]: {results[index-2]}')
+
+        sum = results[index-1] + results[index-2]
+        print(f'sum: {sum} \t\tresults: {results}')
+
+        results.append(sum)
+        index += 1
+
+fibonacci(7)
