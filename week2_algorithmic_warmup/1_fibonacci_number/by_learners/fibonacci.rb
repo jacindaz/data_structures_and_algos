afb70a016e1cf9159b2026bba63bf866a012a2be
Jacinda Zhong <jacindaz@gmail.com>
2018-03-01 13:11:55 -0800

    Non-recursive fibonacci problem

--- week2_algorithmic_warmup/1_fibonacci_number/by_learners/fibonacci.rb
+++ week2_algorithmic_warmup/1_fibonacci_number/by_learners/fibonacci.rb
@@ -12,3 +12,28 @@ if __FILE__ == $0
   n = gets.to_i
   puts "#{calc_fib(n)}"
 end
+
+def fibonacci(n)
+  results = []
+  results[0] = 0
+  results[1] = 1
+
+  index = 2
+  (index..n).to_a.each do |number|
+    add_prev_numbers = results[index - 1] + results[index - 2]
+
+    puts "\n==================="
+    puts "number: #{number}"
+    puts "results[index-1]: #{results[index-1]}, results[index-2]: #{results[index-2]}"
+    puts "add_prev_numbers: #{add_prev_numbers}"
+    puts "===================\n"
+
+    index += 1
+    results << add_prev_numbers
+  end
+
+  puts "results: #{results}"
+  results
+end
+
+fibonacci(10)
