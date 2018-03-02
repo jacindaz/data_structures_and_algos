dbee13ba968896b84f051c61c946fd196896b3cd
Jacinda Zhong <jacindaz@gmail.com>
2018-03-02 13:09:02 -0800

    fix solution, squash later

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,18 +1,18 @@
-# Uses python3
-
 n = int(input())
 a = [int(x) for x in input().split()]
 
 largest_number = 0
 second_largest = 0
 
-for i in range(n):
+print(f'n: {n} \ta: {a}')
+
+for i in a:
     if i > largest_number:
         print(f'i: {i} \tlargest_number: {largest_number}')
         largest_number = i
 
 print('\n')
-for i in range(n):
+for i in a:
     if i == largest_number:
         print(f'i: {i} \tlargest_number: {largest_number}')
         pass
@@ -20,6 +20,4 @@ for i in range(n):
         print(f'i: {i}, second_largest: {second_largest} \tlargest_number: {largest_number}')
         second_largest = i
 
-product = largest_number*second_largest
-print(f'\nproduct: {product}')
-return product
+print(largest_number*second_largest)
