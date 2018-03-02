312b57c4f24a2bf3e04ba0af7bbf62e4b948fd10
Jacinda Zhong <jacindaz@gmail.com>
2018-03-02 12:49:36 -0800

    Week 1, problem 2 max pairwise product problem

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,13 +1,25 @@
 # Uses python3
+
 n = int(input())
 a = [int(x) for x in input().split()]
-assert(len(a) == n)
 
-result = 0
+largest_number = 0
+second_largest = 0
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
+for i in range(n):
+    if i > largest_number:
+        print(f'i: {i} \tlargest_number: {largest_number}')
+        largest_number = i
 
-print(result)
+print('\n')
+for i in range(n):
+    if i == largest_number:
+        print(f'i: {i} \tlargest_number: {largest_number}')
+        pass
+    elif i > second_largest:
+        print(f'i: {i}, second_largest: {second_largest} \tlargest_number: {largest_number}')
+        second_largest = i
+
+product = largest_number*second_largest
+print(f'\nproduct: {product}')
+return product
