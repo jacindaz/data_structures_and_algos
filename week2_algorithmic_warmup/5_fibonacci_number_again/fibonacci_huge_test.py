b667be895c6f4b47ba641b2fddf05a194e6e2468
Jacinda Zhong <jacindaz@gmail.com>
2018-03-05 15:14:25 -0800

    Week 2, prob 5: more tests, move constant to inside the test class

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge_test.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge_test.py
@@ -2,11 +2,14 @@ import unittest
 from fibonacci_huge import fibonacci_huge
 import pdb
 
-PERIOD_LENGTHS = {
-    2: 3, 3: 8, 4: 6, 5: 20,
-    6: 24, 7: 16, 8: 12, 9: 24, 10: 60
-}
 class TestStringMethods(unittest.TestCase):
+    PERIOD_LENGTHS = {
+        2: 3, 3: 8, 4: 6, 5: 20,
+        6: 24, 7: 16, 8: 12, 9: 24, 10: 60,
+        11: 10, 12: 24, 13: 28, 14: 48, 15: 40,
+        16: 24, 17: 36, 18: 24, 19: 18,
+        20: 60, 21: 16, 22: 30, 23: 48, 24: 24,
+    }
 
     PERIOD_VALUES = {
         2: '011', 3: '01120221', 4: '011231', 5: '01123033140443202241',
@@ -20,6 +23,25 @@ class TestStringMethods(unittest.TestCase):
     def test_period_length(self):
         random_fibonacci_n = 500
 
-        for key, value in PERIOD_LENGTHS.items():
-            pisano_period = fibonacci_huge(random_fibonacci_n, key)
-            self.assertEqual(len(pisano_period), value)
+        for modulo, expected_period_length in self.PERIOD_LENGTHS.items():
+            pisano_period = fibonacci_huge(random_fibonacci_n, modulo)
+            self.assertEqual(len(pisano_period), expected_period_length)
+
+    def test_period_values(self):
+        random_fibonacci_n = 500
+
+        for modulo, expected_period_value in self.PERIOD_VALUES.items():
+            pisano_period = fibonacci_huge(random_fibonacci_n, modulo)
+            self.assertEqual(''.join(pisano_period), expected_period_value)
+
+    def test_larger_peroid_lengths(self):
+        lengths = {
+            109: 108, 110: 60, 111: 152, 112: 48, 113: 76,
+            114: 72, 115: 240, 116: 42, 117: 168, 118: 174,
+            119: 144, 120: 120
+        }
+        random_fibonacci_n = 500
+
+        for modulo, expected_period_length in lengths.items():
+            pisano_period = fibonacci_huge(random_fibonacci_n, modulo)
+            self.assertEqual(len(pisano_period), expected_period_length)
