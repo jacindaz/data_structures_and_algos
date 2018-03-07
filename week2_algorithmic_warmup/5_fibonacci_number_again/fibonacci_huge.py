a04e3c52204b74304aa7c473c37527aef6aabfa2
Jacinda Zhong <jacindaz@gmail.com>
2018-03-07 11:17:50 -0800

    Week 2, prob 5: fixed finding pisano period method

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -20,28 +20,39 @@ def get_fibonacci_huge_naive(n, m):
 #     print(get_fibonacci_huge_naive(n, m))
 
 def fibonacci(n):
-    results = [0, 1]
-    index = 2
+    if n == 0:
+        return [0]
+    else:
+        results = [0, 1]
+        index = 2
 
-    for x in range(2,n+1):
-        sum = results[index-1] + results[index-2]
-        results.append(sum)
-        index += 1
+        for x in range(2,n+1):
+            sum = results[index-1] + results[index-2]
+            results.append(sum)
+            index += 1
 
     return results
 
 def find_pisano_period_length(fibonacci_num, mod):
-    fibo = fibonacci(fibonacci_num)
     pisano_period = []
     pisano_period_length = 0
     num_times_seen_011 = 0
 
-    if pisano_period == ['0', '1', '1']:
-        num_times_seen_011 += 1
-
     index = 0
-    while pisano_period != ['0', '1', '1'] and num_times_seen_011 < 2:
+    while num_times_seen_011 < 2:
+        if pisano_period == ['0', '1', '1']:
+            num_times_seen_011 += 1
+
+        if num_times_seen_011 == 2:
+            break
+
         mod_result = fibonacci(index)[-1] % mod
+
+        print('\n------------------')
+        print(f'pisano_period: {pisano_period}, pisano_period_length: {pisano_period_length}')
+        print(f'num_times_seen_011: {num_times_seen_011}')
+        print(f'mod_result = fibonacci(index)[-1] % mod: {mod_result}')
+        print('------------------\n')
 
         if len(pisano_period) == 3:
             pisano_period.append(str(mod_result))
