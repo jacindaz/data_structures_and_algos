f44ff853787d8d51dd7eec4e9df1902327eab31a
Jacinda Zhong <jacindaz@gmail.com>
2018-03-10 15:14:52 -0800

    Week3, prob1: money change problem solution

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,16 @@
 # Uses python3
 import sys
 
-def get_change(m):
-    #write your code here
-    return m
+def get_change(money):
+    total_coins = 0
+
+    for coin in [10, 5, 1]:
+        if money/coin >= 1:
+            num_tens = money//coin
+            total_coins += num_tens
+            money -= num_tens*coin
+
+    return total_coins
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
