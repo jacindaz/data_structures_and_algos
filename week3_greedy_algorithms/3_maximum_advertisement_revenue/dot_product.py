d659f1c9a79c2626aaec1f09a3aa793f31f507bc
Jacinda Zhong <jacindaz@gmail.com>
2018-03-11 21:07:42 -0700

    Wk3, p3: maximum dot product

--- week3_greedy_algorithms/3_maximum_advertisement_revenue/dot_product.py
+++ week3_greedy_algorithms/3_maximum_advertisement_revenue/dot_product.py
@@ -1,5 +1,4 @@
 #Uses python3
-
 import sys
 
 def max_dot_product(a, b):
@@ -9,11 +8,21 @@ def max_dot_product(a, b):
         res += a[i] * b[i]
     return res
 
+def greedy_dot_product(profit_per_click, avg_clicks):
+    profit_per_click = sorted(profit_per_click, reverse=True)
+    avg_clicks = sorted(avg_clicks, reverse=True)
+
+    products = []
+    for index, click in enumerate(profit_per_click):
+        products.append(click * avg_clicks[index])
+
+    products = sorted(products, reverse=True)
+    return sum(products)
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
     a = data[1:(n + 1)]
     b = data[(n + 1):]
-    print(max_dot_product(a, b))
-    
+    print(bananas(a, b))
