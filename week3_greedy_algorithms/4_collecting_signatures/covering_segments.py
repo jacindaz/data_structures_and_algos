087c3683526fabcc638e3c187788553d9267823e
Jacinda Zhong <jacindaz@gmail.com>
2018-03-13 13:18:08 -0700

    Wk3,p4: first sort segments by starting coordinate

--- week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
@@ -4,19 +4,27 @@ from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
+
 def optimal_points(segments):
+    sorted_points = sorted(segments, key=lambda segment: segment.start)
+
+    print('\n-----------------------')
+    print(f'sorted points: {sorted_points}')
+    print('-----------------------\n')
+
     points = []
-    #write your code here
     for s in segments:
         points.append(s.start)
         points.append(s.end)
     return points
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *data = map(int, input.split())
-    segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
-    points = optimal_points(segments)
-    print(len(points))
-    for p in points:
-        print(p, end=' ')
+optimal_points([Segment(2,5), Segment(1,3), Segment(3,6)])
+
+# if __name__ == '__main__':
+#     input = sys.stdin.read()
+#     n, *data = map(int, input.split())
+#     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+#     points = optimal_points(segments)
+#     print(len(points))
+#     for p in points:
+#         print(p, end=' ')
