29cd0b993699cbb20310fec5622058393919b4b3
Jacinda Zhong <jacindaz@gmail.com>
2018-03-13 23:14:19 -0700

    Wk3,p5: max number of prizes
     - for some reason, the trick is to determine the last valid
       number, you can do the current number you are iterating on,
       multiply that by 2, and if it is <= the original input number,
       then you know that you can break out of the loop
     - mathematically this works for some reason, but i'm not sure why

--- week3_greedy_algorithms/5_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/5_maximum_number_of_prizes/different_summands.py
@@ -1,15 +1,43 @@
 # Uses python3
 import sys
 
-def optimal_summands(n):
+def optimal_summands(number):
     summands = []
-    #write your code here
+    for i in range(1, number+1):
+        print(f'i: {i}, number: {number}')
+        print(f'summands: {summands}\n')
+
+        if i * 2 >= number: # is this the last number ?
+            summands.append(number)
+            break
+
+        else:
+            summands.append(i)
+            number = number - i
+            i = i +1
+
     return summands
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    summands = optimal_summands(n)
-    print(len(summands))
-    for x in summands:
-        print(x, end=' ')
+print(optimal_summands(6))
+# 3
+# 1 2 3
+
+# print(optimal_summands(8))
+# 3
+# 1 2 5
+
+# print(optimal_summands(2))
+# 1
+# 2
+
+# print(optimal_summands(4))
+# 2
+# 1 3
+
+# if __name__ == '__main__':
+#     input = sys.stdin.read()
+#     n = int(input)
+#     summands = optimal_summands(n)
+#     print(len(summands))
+#     for x in summands:
+#         print(x, end=' ')
