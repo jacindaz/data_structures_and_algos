b202ac186d552712d2d3c8da019c156b0ec6f31e
Jacinda Zhong <jacindaz@gmail.com>
2018-03-14 12:42:49 -0700

    Wk3,p4: collecting signatures, covering segments

--- week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/4_collecting_signatures/covering_segments.py
@@ -4,21 +4,38 @@ from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
-
 def optimal_points(segments):
-    sorted_points = sorted(segments, key=lambda segment: segment.start)
-
-    print('\n-----------------------')
-    print(f'sorted points: {sorted_points}')
-    print('-----------------------\n')
-
+    sorted_segments = sorted(segments, key=lambda segment: segment.end)
     points = []
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+
+    while len(sorted_segments) > 0:
+        segment = sorted_segments[0]
+        min_right_endpoint = segment.end
+        points.append(min_right_endpoint)
+
+        print(f'\nsorted_segments: {sorted_segments}')
+        print(f'segment: {segment}')
+        print(f'min_right_endpoint: {min_right_endpoint}, points: {points}')
+
+        inner_index = 0
+        while inner_index < len(sorted_segments):
+            current_segment = sorted_segments[inner_index]
+            if min_right_endpoint in range(current_segment.start, current_segment.end+1):
+                print(f'deleting segment: {current_segment}')
+                sorted_segments.remove(current_segment)
+            else:
+                inner_index += 1
+
     return points
 
-optimal_points([Segment(2,5), Segment(1,3), Segment(3,6)])
+
+# print(optimal_points([Segment(1,3), Segment(2,5), Segment(3,6)]))
+# 1
+# 3
+
+# print(optimal_points([Segment(4,7), Segment(1,3), Segment(2,5), Segment(5,6)]))
+# 3 6
+
 
 # if __name__ == '__main__':
 #     input = sys.stdin.read()
