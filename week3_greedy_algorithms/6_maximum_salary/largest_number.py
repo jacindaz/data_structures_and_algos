0f01b28b9e6ba6b67235f06ba1fdde4615b677c2
Jacinda Zhong <jacindaz@gmail.com>
2018-03-14 16:11:09 -0700

    Wk3,p6: implemented largest number algorithm for single digit numbers

--- week3_greedy_algorithms/6_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/6_maximum_salary/largest_number.py
@@ -1,17 +1,55 @@
 #Uses python3
-
 import sys
 
-def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+# Interestingly, for solving this problem, you need to
+# replace the check digit >= maxDigit with a call
+# IsGreaterOrEqual(digit, maxDigit) for an appropriately
+# implemented function IsGreaterOrEqual.
+# For example, IsGreaterOrEqual(2, 21) should return True.
+def largest_number(numbers):
+    answer = ""
+    while len(numbers) > 0:
+        max_digit = float("-inf")
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+        print('\n------------------------------')
+
+        for number in numbers:
+            print(f'number: {number}, numbers: {numbers}')
+            print(f'max digit: {max_digit}, answer: {answer}')
+
+            if number >= max_digit:
+                max_digit = number
+                print(f'new max digit: {max_digit}')
+
+        answer += str(max_digit)
+        numbers.remove(max_digit)
+
+        print(f'appended to answer: {answer}, removed a number: {numbers}')
+        print('------------------------------\n')
+
+    return answer
+
+# print(largest_number([9,4,6,1,9]))
+# 5
+# 9 4 6 1 9
+# Output:
+# 99641
+
+# print(largest_number([23, 39, 92]))
+# 3
+# 23 39 92
+# Output:
+# 923923
+
+print(largest_number([21, 2]))
+# 2
+# 21 2
+# Output:
+# 221
+
+# if __name__ == '__main__':
+#     input = sys.stdin.read()
+#     data = input.split()
+#     a = data[1:]
+#     print(largest_number(a))
+
