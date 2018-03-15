6475349b566db36c5232a476fc79ffa66c8602ce
Jacinda Zhong <jacindaz@gmail.com>
2018-03-14 17:58:58 -0700

    Wk3,p6: works for my test cases, but not for those in the grader :(

--- week3_greedy_algorithms/6_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/6_maximum_salary/largest_number.py
@@ -1,55 +1,39 @@
 #Uses python3
 import sys
 
-# Interestingly, for solving this problem, you need to
-# replace the check digit >= maxDigit with a call
-# IsGreaterOrEqual(digit, maxDigit) for an appropriately
-# implemented function IsGreaterOrEqual.
-# For example, IsGreaterOrEqual(2, 21) should return True.
+def is_greater_or_equal(number, max_number):
+    shortest_length = 0
+    if len(str(number)) <= len(str(max_number)):
+        shortest_length = len(str(number))
+    else:
+        shortest_length = len(str(max_number))
+
+    truncated_number = int(str(number)[:shortest_length])
+    truncated_max_number = int(str(max_number)[:shortest_length])
+
+    if truncated_number >= truncated_max_number:
+        return True
+    else:
+        return False
+
 def largest_number(numbers):
     answer = ""
     while len(numbers) > 0:
-        max_digit = float("-inf")
-
-        print('\n------------------------------')
-
+        max_number = numbers[0]
         for number in numbers:
-            print(f'number: {number}, numbers: {numbers}')
-            print(f'max digit: {max_digit}, answer: {answer}')
+            if is_greater_or_equal(number, max_number):
+                max_number = number
 
-            if number >= max_digit:
-                max_digit = number
-                print(f'new max digit: {max_digit}')
-
-        answer += str(max_digit)
-        numbers.remove(max_digit)
-
-        print(f'appended to answer: {answer}, removed a number: {numbers}')
-        print('------------------------------\n')
+        answer += str(max_number)
+        numbers.remove(max_number)
 
     return answer
 
-# print(largest_number([9,4,6,1,9]))
-# 5
-# 9 4 6 1 9
-# Output:
-# 99641
-
-# print(largest_number([23, 39, 92]))
-# 3
-# 23 39 92
-# Output:
-# 923923
-
-print(largest_number([21, 2]))
-# 2
-# 21 2
-# Output:
-# 221
+print(largest_number([2,8,2,3,6,4,1,1,10,6,3,3,6,1,3,8,4,6,1,10,8,4,10,4,1,3,2,3,2,6,1,5,2,9,8,5,10,8,7,9,6,4,2,6,3,8,8,9,8,2,9,10,3,10,7,5,7,1,7,5,1,4,7,6,1,10,5,4,8,4,2,7,8,1,1,7,4,1,1,9,8,6,5,9,9,3,7,6,3,10,8,10,7,2,5,1,1,9,9,5]))
+# 9999999998888888888887777777776666666666555555554444444443333333333222222222111111111111111101010101010101010
 
 # if __name__ == '__main__':
 #     input = sys.stdin.read()
 #     data = input.split()
 #     a = data[1:]
 #     print(largest_number(a))
-
