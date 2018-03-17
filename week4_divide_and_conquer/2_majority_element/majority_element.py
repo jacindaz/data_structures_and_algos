319f43d54adccfaebe2797182684f4d6dbd91710
Jacinda Zhong <jacindaz@gmail.com>
2018-03-17 15:13:11 -0700

    Wk4,p2: majority element problem, without using divide and conquer

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,13 +1,23 @@
 # Uses python3
 import sys
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+def get_majority_element(numbers, left, right):
+    max_count = 0
+    results = {}
+    majority_element = -1
+
+    for number in numbers:
+        results[number] = 0
+
+    for number in numbers:
+        results[number] += 1
+        if results[number] > max_count:
+            max_count = results[number]
+
+    if max_count > (len(numbers)/2):
+        majority_element = 1
+
+    return majority_element
 
 if __name__ == '__main__':
     input = sys.stdin.read()
