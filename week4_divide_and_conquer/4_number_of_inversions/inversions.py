a63eaba231f9cf7f46620bc809260d600c1e880d
Jacinda Zhong <jacindaz@gmail.com>
2018-03-19 23:38:55 -0700

    wk4,p4: some fixes to merge sort, append rest of array, instead of just first element

--- week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -17,14 +17,16 @@ def merge(array1, array2):
         if first_el_a1 <= first_el_a2:
             merged_array.append(first_el_a1)
             array1 = array1[1:]
-        else:
+        elif first_el_a1 > first_el_a2:
             merged_array.append(first_el_a2)
             array2 = array2[1:]
 
     if len(array1) > 0:
-        merged_array.append(array1[0])
+        print(f'\nthere"s some left overs! array 1: {array1}\n')
+        merged_array = merged_array + array1
     elif len(array2) > 0:
-        merged_array.append(array2[0])
+        print(f'\nthere"s some left overs! array 2: {array2}\n')
+        merged_array = merged_array + array2
 
     print('\n-----------------------------------\n')
     return merged_array
@@ -33,17 +35,21 @@ def merge(array1, array2):
 # print(merge([3,27,38,43], [9,10,82]))
 
 
-def merge_sort(array_to_sort, index=0):
+def merge_sort(array_to_sort):
     array_length = len(array_to_sort)
 
     if array_length == 1:
         return array_to_sort
 
     midpoint = array_length//2
-    array_left_half = merge_sort(array_to_sort[:midpoint], index+1)
-    array_right_half = merge_sort(array_to_sort[midpoint:], index+1)
+
+    array_left_half = merge_sort(array_to_sort[:midpoint])
+    array_right_half = merge_sort(array_to_sort[midpoint:])
+
     sorted_array = merge(array_left_half, array_right_half)
 
     return sorted_array
 
-print(merge_sort([38,27,43,39,82,10]))
+# print(merge_sort([38,27,43,39,82,10]))
+# print(merge_sort([2,3,9,2,9]))
+# print(merge_sort([2,2,2,2,2,2,2,2,99,2,2,1,0,2,2,2,2,2,-44]))
