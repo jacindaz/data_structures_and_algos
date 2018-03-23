65b1cb7ab37ff2ac6cda2096d315330967573358
Jacinda Zhong <jacindaz@gmail.com>
2018-03-23 12:08:28 -0700

    Wk4,p5: solution for finding if points exist on particular segments

--- week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -1,36 +1,49 @@
 # Uses python3
 import sys
+import collections
 
 def fast_count_segments(starts, ends, points):
+    left_label, point_label, right_label = (1, 2, 3)
     count = [0] * len(points)
-    #write your code here
-    return count
 
-# print(fast_count_segments([0,7],[5,10],[1,6,11]))
+    points_map = collections.defaultdict(set)
 
-def naive_count_segments(starts, ends, points):
-    count = [0] * len(points)
+    pairs = []
+    for i in starts:
+        pairs.append((i, left_label))
+
+    for i in ends:
+        pairs.append((i, right_label))
+
     for i in range(len(points)):
-        for j in range(len(starts)):
+        point = points[i]
+        pairs.append((point, point_label))
+        points_map[point].add(i)
 
-            print('\n-----------------------\n')
-            print(f'i: {i}, j: {j}, count: {count}')
-            print(f'starts[j]: {starts[j]}, points[i]: {points[i]}, ends[j]: {ends[j]}')
-            print(f'points: {points}')
+    sorted_pairs = sorted(pairs, key=lambda p: (p[0], p[1]))
 
-            if starts[j] <= points[i] <= ends[j]:
-                count[i] += 1
+    coverage = 0
+    for pair in sorted_pairs:
+        if pair[1] == left_label:
+            coverage += 1
+        if pair[1] == right_label:
+            coverage -= 1
+        if pair[1] == point_label:
+            indices = points_map[pair[0]]
+
+            for i in indices:
+                count[i] = coverage
+
     return count
 
-# print(naive_count_segments([0,7],[5,10],[1,6,11]))
-print(naive_count_segments([0,5],[5,10],[1,5,11]))
-
-
-
-# Input:
-# 2 3
-# 0 5
-# 7 10
-# 1 6 11
-# Output:
-# 1 0 0
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n = data[0]
+    m = data[1]
+    starts = data[2:2 * n + 2:2]
+    ends   = data[3:2 * n + 2:2]
+    points = data[2 * n + 2:]
+    count = fast_count_segments(starts, ends, points)
+    for x in count:
+        print(x, end=' ')
