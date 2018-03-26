4cc5f451f75beff016ca0a3abec5554894c99054
Jacinda Zhong <jacindaz@gmail.com>
2018-03-26 14:01:14 -0700

    Wk5,p1: add back in print statements and more test cases

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -9,26 +9,29 @@ def get_change(money):
 
         for coin in [1,3,4]:
             if m >= coin:
-                # print('\n---------------\n')
-                # print(f'm: {m}, coin: {coin}')
+                print('\n---------------\n')
+                print(f'm: {m}, coin: {coin}')
 
                 num_coins = min_num_coins[m-coin] + 1
 
-                # print(f'num_coins: {num_coins}, min_num_coins: {min_num_coins}')
+                print(f'num_coins: {num_coins}, min_num_coins: {min_num_coins}')
 
                 if num_coins < min_num_coins[m]:
                     min_num_coins[m] = num_coins
-                    # print('num_coins < min_num_coins[m] is true! ')
-                    # print(f'num_coins: {num_coins}, min_num_coins: {min_num_coins}')
+                    print('num_coins < min_num_coins[m] is true! ')
+                    print(f'num_coins: {num_coins}, min_num_coins: {min_num_coins}')
 
 
     return min_num_coins[-1]
 
-# print(get_change(34))
+print(get_change(5)) # 2
+print(get_change(4)) # 1
+# print(get_change(34)) # 9
+# print(get_change(2)) # 2
 
-if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+# if __name__ == '__main__':
+#     m = int(sys.stdin.read())
+#     print(get_change(m))
 
 
 # Input:
