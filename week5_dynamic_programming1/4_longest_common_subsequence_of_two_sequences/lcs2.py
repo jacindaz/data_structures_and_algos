350af95940492620c6b2e4daa3f8976b5d501e7b
Jacinda Zhong <jacindaz@gmail.com>
2018-04-01 16:36:03 -0700

    Wk5,p4: next iteration of longest common sequence, works for dupes now, still failing grader though :(

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,84 +1,86 @@
 #Uses python3
 import sys
+
 def starter_grid_x(sequence1_x, sequence2_y):
     grid = []
-
     y_index = 0
+
     for x_index, seq1_x_number in enumerate(sequence1_x):
-        print('\n------------------\n')
-        print(f'x_index: {x_index}, y_index: {y_index}')
-        print(f'grid: {grid}')
+        seq2_y_number = sequence2_y[y_index]
 
         if x_index == 0 and y_index == 0:
-            grid.append([0])
-
-            print('x and y index are 0')
-            print(f'y_index: {y_index}')
-        else:
-            seq2_y_number = sequence2_y[y_index]
-            previous_number = grid[y_index][x_index-1]
-
-            print(f'\nx and y index are not 0')
-            print(f'seq1_x_number: {seq1_x_number}, seq2_y_number: {seq2_y_number}')
-            print(f'previous_number: {previous_number}')
-            print(f'grid[y_index]: {grid[y_index]}')
-            print(f'\ngrid: {grid}')
-
             if seq1_x_number == seq2_y_number:
-                grid[y_index].append(previous_number+1)
+                grid.append([1])
             else:
-                grid[y_index].append(previous_number)
+                grid.append([0])
+        else:
+            previous_number = grid[y_index][x_index-1]
+            grid[y_index].append(previous_number)
 
-        print(f'newly appended grid: {grid}')
     return grid
 
-print(starter_grid_x([2,7,8,3], [5,2,8,7,3]))
+# print(starter_grid_x([2,1,3,3,2], [2,5,2,2,3]))
 
 def starter_grid(sequence1_x, sequence2_y):
     grid = starter_grid_x(sequence1_x, sequence2_y)
-
     x_index = 0
+
     for y_index in range(1, len(sequence2_y)):
         seq2_y_number = sequence2_y[y_index]
-        print('\n------------------\n')
-        print(f'x_index: {x_index}, y_index: {y_index}')
-        print(f'grid: {grid}')
-
         seq1_x_number = sequence1_x[x_index]
         previous_number = grid[y_index-1][x_index]
 
-        print(f'\nx and y index are not 0')
-        print(f'seq1_x_number: {seq1_x_number}, seq2_y_number: {seq2_y_number}')
-        print(f'previous_number: {previous_number}')
-        print(f'\ngrid: {grid}')
-
-        if seq1_x_number == seq2_y_number:
-            grid.append([previous_number+1])
+        if grid[0][0] == 1:
+            grid.append([1])
         else:
-            grid.append([previous_number])
+            if seq1_x_number == seq2_y_number:
+                grid.append([previous_number+1])
+            else:
+                grid.append([previous_number])
 
-        print(f'newly appended grid: {grid}')
+    return grid
+# print(starter_grid([2,1,3,3,2], [2,5,2,2,3]))
+# print(starter_grid([1,3,2], [2,5,2,2,3])) # 1
+# print(starter_grid([5,3,2], [2,5,2,2,3])) # 2
+
+
+def construct_full_matrix(sequence1_x, sequence2_y):
+    grid = starter_grid(sequence1_x, sequence2_y)
+
+    for x_index in range(1,len(sequence1_x)):
+        seq1_number_x = sequence1_x[x_index]
+
+        for y_index in range(1, len(sequence2_y)):
+            seq2_number_y = sequence2_y[y_index]
+
+            above = grid[y_index-1][x_index]
+            diagonal = grid[y_index-1][x_index-1]
+            left = grid[y_index][x_index-1]
+
+            longest_sequence = max(above, diagonal, left)
+
+            if seq1_number_x == seq2_number_y:
+                # grid[y_index].append(longest_sequence+1)
+                grid[y_index].append(diagonal+1)
+            elif seq1_number_x != seq2_number_y:
+                grid[y_index].append(longest_sequence)
+                # grid[y_index].append(diagonal)
 
     return grid
 
-print(starter_grid([2,7,8,3], [5,2,8,7,3]))
-# print(starter_grid([5,2,8,7,3], [2,7,8,3]))
+# print(construct_full_matrix([2,1,3,3,2], [2,5,2,2,3])) # 2
+# print(construct_full_matrix([1,3,2], [2,5,2,2,3])) # 1
+# print(construct_full_matrix([5,3,2], [2,5,2,2,3])) # 2
 
-def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+def lcs2(seq1, seq2):
+    grid = construct_full_matrix(seq1, seq2)
+    result = grid[-1][-1]
+    return result
 
-    n = data[0]
-    data = data[1:]
-    a = data[:n]
 
-    data = data[n:]
-    m = data[0]
-    data = data[1:]
-    b = data[:m]
-
-    print(lcs2(a, b))
+# print(lcs2([2,7,5], [2,5])) # 2
+# print(lcs2([2,7,8,3], [5,2,8,7,3])) # 3
+# print(lcs2([7], [1,2,3,4])) # 0
+# print(lcs2([1,2,3,4], [5,6,7,10,2,2,2,2,3])) # 2
+# print(lcs2([2,1,3,3,2], [2,5,2,2,3])) # 2
