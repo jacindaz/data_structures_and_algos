35ac7668a522fe8e4ef8bbabb589cf592e95ceaf
Jacinda Zhong <jacindaz@gmail.com>
2018-04-04 20:09:34 -0700

    Wk5,p4: longest common sequence solution! with stress test

--- week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,74 +1,5 @@
 # Uses python3
 import sys
-from random import *
-
-def brute_force(s1, s2, i, j):
-    if(i == -1 or j == -1):
-        return 0
-    if(s1[i] == s2[j]):
-        return 1 + brute_force(s1, s2, i-1, j-1)
-    return max(brute_force(s1, s2, i-1, j), brute_force(s1, s2, i, j-1))
-
-# string1, string2 = [[2,7,5], [2,5]] # 2
-# string1, string2 = [[2,7,8,3], [5,2,8,7,3]] # 3
-# string1, string2 = [[7], [1,2,3,4]] # 0
-# string1, string2 = [[1,2,3,4], [5,6,7,10,2,2,2,2,3]] # 2
-# string1, string2 = [[2,1,3,3,2], [2,5,2,2,3]] # 2
-# string1, string2 = [[1,2,2,2,6], [5,6,7,10,2,2,2,2,3]] # 3
-# print(brute_force(string1, string2, len(string1)-1, len(string2)-1))
-
-def stress_testing_random_input():
-    s1_length = randint(1, 20)
-    s2_length = randint(1, 20)
-    # print(f's1_length: {s1_length}, s2_length: {s2_length}')
-
-    if s1_length > 3 and s2_length > 3:
-        num_dupes = randint(2, min(s1_length, s2_length))
-        dupe_value = randint(1,50)
-        # print(f'num_dupes: {num_dupes}, dupe_value: {dupe_value}')
-    else:
-        num_dupes = randint(1, 2)
-        dupe_value = randint(1,50)
-        # print(f'num_dupes: {num_dupes}, dupe_value: {dupe_value}')
-
-    s1 = []
-    for n in range(1,s1_length-num_dupes):
-        s1.append(randint(1,50))
-    # print(f'\ns1: {s1}, s1_length-num_dupes: {s1_length-num_dupes}')
-
-    s2 = []
-    for n in range(1,s2_length-num_dupes):
-        s2.append(randint(1,50))
-    # print(f's2: {s2}, s2_length-num_dupes: {s2_length-num_dupes}')
-
-    # s1: append dupes at random index
-    for n in range(1,num_dupes):
-        # print('------------------------------------------------------')
-
-        random_index = randint(0,s1_length-1)
-        # print(f'random_index: {random_index}, s1_length: {s1_length}')
-        # print(f'old s1: {s1}')
-
-        s1.insert(random_index, dupe_value)
-        # print(f's1 with new dupe value: {s1}')
-
-    # s2: append dupes at random index
-    for n in range(1,num_dupes):
-        # print('------------------------------------------------------')
-
-        random_index = randint(0,s2_length-1)
-        # print(f'random_index: {random_index}, s2_length: {s2_length}')
-        # print(f'old s2: {s2}')
-
-        s2.insert(random_index, dupe_value)
-        # print(f's2 with new dupe value: {s2}')
-
-    # print(f'\ns1 length: {s1_length} \ts1: {s1}')
-    # print(f's2 length: {s2_length} \ts2: {s2}')
-
-    return [s1, s2]
-
-# print(stress_testing_random_input())
 
 def starter_grid_x(sequence1_x, sequence2_y):
     grid = []
@@ -92,9 +23,6 @@ def starter_grid_x(sequence1_x, sequence2_y):
 
     return grid
 
-# print(starter_grid_x([2,1,3,3,2], [2,5,2,2,3]))
-# print(starter_grid_x([16,49,19,16,9,21,8,31,35,34],[34]))
-
 def starter_grid(sequence1_x, sequence2_y):
     grid = starter_grid_x(sequence1_x, sequence2_y)
     x_index = 0
@@ -113,12 +41,6 @@ def starter_grid(sequence1_x, sequence2_y):
                 grid.append([previous_number])
 
     return grid
-# print(starter_grid([2,1,3,3,2], [2,5,2,2,3]))
-# print(starter_grid([1,3,2], [2,5,2,2,3])) # 1
-# print(starter_grid([5,3,2], [2,5,2,2,3])) # 2
-# print(starter_grid([18,23,12,4,46,14,32,35,9,47,4,19,16,16,4,39,15,4], [4,4,4]))
-# print(starter_grid([4,4,4], [18,23,4,46,4,4,15,4]))
-
 
 def construct_full_matrix(sequence1_x, sequence2_y):
     grid = starter_grid(sequence1_x, sequence2_y)
@@ -136,71 +58,28 @@ def construct_full_matrix(sequence1_x, sequence2_y):
             longest_sequence = max(above, diagonal, left)
 
             if seq1_number_x == seq2_number_y:
-                # grid[y_index].append(longest_sequence+1)
                 grid[y_index].append(diagonal+1)
             elif seq1_number_x != seq2_number_y:
                 grid[y_index].append(longest_sequence)
-                # grid[y_index].append(diagonal)
 
     return grid
-
-# print(construct_full_matrix([2,1,3,3,2], [2,5,2,2,3])) # 2
-# print(construct_full_matrix([1,3,2], [2,5,2,2,3])) # 1
-# print(construct_full_matrix([5,3,2], [2,5,2,2,3])) # 2
-# print(construct_full_matrix([16,49,19,16,9,21,8,31,35,34],[34]))
-
-# START HERE
-#  => these two return different results... why???
-# print(construct_full_matrix([4,4,4],[18,23,12,4,46,14,32,35,9,47,4,19,16,16,4,39,15,4]))
-# print(construct_full_matrix([18,23,12,4,46,14,32,35,9,47,4,19,16,16,4,39,15,4], [4,4,4]))
-
-# print(construct_full_matrix([18,23,4,46,4,4,15,4], [4,4,4]))
-
-# print(construct_full_matrix([4,4,4], [18,23,4,46,4,4,15,4]))
-# [
-#     [0, 0, 0],
-#     [0, 0, 0],
-#     [1, 1, 1],
-#     [1, 1, 1],
-#     [2, 2, 2],
-#     [3, 3, 3],
-#     [3, 3, 3],
-#     [4, 4, 4]
-# ]
-
 
 def lcs2(seq1, seq2):
     grid = construct_full_matrix(seq1, seq2)
     result = grid[-1][-1]
     return result
 
-# print(lcs2([2,7,5], [2,5])) # 2
-# print(lcs2([2,7,8,3], [5,2,8,7,3])) # 3
-# print(lcs2([7], [1,2,3,4])) # 0
-# print(lcs2([1,2,3,4], [5,6,7,10,2,2,2,2,3])) # 2
-# print(lcs2([1,2,2,2,6], [5,6,7,10,2,2,2,2,3])) # 3
-# print(lcs2([2,1,3,3,2], [2,5,2,2,3])) # 2
-# print(lcs2([16,49,19,16,9,21,8,31,35,34],[34]))
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
 
+    n = data[0]
+    data = data[1:]
+    a = data[:n]
 
-def run_stress_test(num_iterations):
-    for n in range(1,num_iterations):
-        print('----------------------')
+    data = data[n:]
+    m = data[0]
+    data = data[1:]
+    b = data[:m]
 
-        seq1, seq2 = stress_testing_random_input()
-        print(f'seq1: {seq1}')
-        print(f'seq2: {seq2}')
-
-        if len(seq1) == 0 or len(seq2) == 0:
-            next
-        else:
-            brute_force_result = brute_force(seq1, seq2, len(seq1)-1, len(seq2)-1)
-            optimized_result = lcs2(seq1, seq2)
-
-            if brute_force_result != optimized_result:
-                print('\e[1;31mdoes not match!!!\e[1;31m')
-                print(f'brute_force_result: {brute_force_result} \toptimized_result: {optimized_result}')
-                print(f'seq1: {seq1}')
-                print(f'seq2: {seq2}')
-
-run_stress_test(20)
+    print(lcs2(a, b))
