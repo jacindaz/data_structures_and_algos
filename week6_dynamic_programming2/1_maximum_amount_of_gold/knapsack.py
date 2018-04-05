4f4e143729421f0a2d2028723335052d04f2357c
Jacinda Zhong <jacindaz@gmail.com>
2018-04-05 12:44:00 -0700

    Wk6,p1: greedy solution (which doesn't always work) for knapsack problem

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,27 @@
 # Uses python3
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
+def greedy_solution(max_capacity, items):
+    result=0
+    items = sorted(items, reverse=True)
+    print(f'items: {items}')
+
+    for item in items:
+        print('-----------------------')
+        print(f'item: {item}, items: {items}')
+
+        if max_capacity > 0:
+            max_item = max(items)
+            print(f'max_item: {max_item}')
+
+            if max_capacity - max_item >= 0:
+                result += max_item
+                max_capacity -= max_item
+                items.remove(max_item)
+
+                print(f'result: {result}, max_capacity: {max_capacity}')
+                print(f'updated items: {items}')
+
     return result
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+print(greedy_solution(10, [1,4,8]))
