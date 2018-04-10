9c682d340d16b10c28e26bb5a8b5f265af4b90cf
Jacinda Zhong <jacindaz@gmail.com>
2018-04-10 11:31:33 -0700

    Wk6,p1: DP knapsack, code for starter grid

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -4,24 +4,32 @@ import sys
 def greedy_solution(max_capacity, items):
     result=0
     items = sorted(items, reverse=True)
-    print(f'items: {items}')
 
     for item in items:
-        print('-----------------------')
-        print(f'item: {item}, items: {items}')
-
         if max_capacity > 0:
             max_item = max(items)
-            print(f'max_item: {max_item}')
-
             if max_capacity - max_item >= 0:
                 result += max_item
                 max_capacity -= max_item
                 items.remove(max_item)
 
-                print(f'result: {result}, max_capacity: {max_capacity}')
-                print(f'updated items: {items}')
-
     return result
 
-print(greedy_solution(10, [1,4,8]))
+def starter_grid(max_capacity, items):
+    grid = []
+
+    # fill in top row with zeroes
+    for n in range(0,max_capacity+1):
+        if n == 0:
+            grid.append([0])
+        else:
+            grid[0].append(0)
+
+    # fill in y-axis columns
+    for n in range(1,len(items)+1):
+        grid.append([0])
+
+    return grid
+
+print(starter_grid(10, [1,4,8]))
+
