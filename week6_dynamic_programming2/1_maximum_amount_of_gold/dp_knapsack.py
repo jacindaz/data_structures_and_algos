da0c70aedfc785ca44507aed567ee20691f924fd
Jacinda Zhong <jacindaz@gmail.com>
2018-04-11 14:33:25 -0700

    Wk6,p1: knapsack dynamic programming solution

--- week6_dynamic_programming2/1_maximum_amount_of_gold/dp_knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/dp_knapsack.py
@@ -1,57 +1,34 @@
 # import ipdb
 
-def starter_grid(max_capacity, items):
-    grid = []
 
-    # fill in top row with zeroes
-    for n in range(0,max_capacity+1):
-        if n == 0:
-            grid.append([0])
-        else:
-            grid[0].append(0)
+def knapsack(max_capacity, items):
+    # assigning for understanding purposes
+    # since item weights and values are the same (because it's gold)
+    values = items
+    weights = items
 
-    # fill in y-axis columns
-    for n in range(1,len(items)+1):
-        grid.append([0])
+    num_items = len(items)
+    max_capacities_grid = [[0 for x in range(max_capacity+1)] for x in range(num_items+1)]
+    print(f'max_capacities_grid: {max_capacities_grid}')
 
-    return grid
-
-def knapsack_dp_solution(max_capacity, items):
-    grid = starter_grid(max_capacity, items)
-    print(f'starter_grid: {grid}')
-    combination_available_capacities = []
-
-    for item_index, current_item in enumerate(items, start=1):
+    for item_index in range(1,num_items+1):
+        current_item = items[item_index-1]
         for capacity in range(1,max_capacity+1):
-            print('-----------------')
-            print(f'item_index: {item_index}, current_item: {current_item}')
-            print(f'capacity: {capacity}')
+            print('-------------')
+            print(f'item_index: {item_index}, capacity: {capacity}, current_item: {current_item}')
+            print(f'max_capacities_grid: {max_capacities_grid}')
 
-            # if capacity of all available items
-            # is less than max_capaity
-            # append to item
-            if current_item == capacity:
-                grid[item_index].append(current_item)
-                print('\nadding to the grid. current_item <= capacity')
-                print(f'grid: {grid}')
-
-            # if capacity-current_item is > 0
-            # can i add another item ? if so, which item?
-            elif (capacity - current_item) > 0:
-                print('something!')
-
-            # if not... ? take capacity from item above
+            if item_index == 0 or capacity == 0:
+                max_capacities_grid[item_index][capacity] = 0
+            elif weights[item_index-1] <= capacity:
+                max_capacities_grid[item_index][capacity] = max(
+                    values[item_index-1] + max_capacities_grid[item_index-1][capacity-weights[item_index-1]],
+                    max_capacities_grid[item_index-1][capacity]
+                )
             else:
-                item_from_above = grid[item_index-1][capacity]
-                grid[item_index].append(item_from_above)
+                max_capacities_grid[item_index][capacity] = max_capacities_grid[item_index-1][capacity]
 
-    return grid
+    print(f'\nfinal grid: {max_capacities_grid}\n')
+    return max_capacities_grid[num_items][max_capacity]
 
-print(knapsack_dp_solution(10, [1,4,8])) # 9
-
-[
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4],
-    [0, 1, 1, 1, 4, 4, 4, 4, 8, 8, 8]
-]
+print(knapsack(10, [2,4,7]))
