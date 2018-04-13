5f56f0b36838f660246039f4f9c595caa77d41bf
Jacinda Zhong <jacindaz@gmail.com>
2018-04-12 19:56:48 -0700

    Wk6,p2: partition problem, rename variables and add print statements to brute force solution

--- week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -1,20 +1,23 @@
 # Uses python3
 import sys
 import itertools
+# import ipdb
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
+def partition3(array):
+    count = 0
+    for something in itertools.product(range(3), repeat=len(array)):
         sums = [None] * 3
+        print('---------------------------')
+        print(f'something: {something}, array: {array}')
+
         for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+            sums[i] = sum(array[k] for k in range(len(array)) if something[k] == i)
+            print(f'i: {i}, sums: {sums}')
 
         if sums[0] == sums[1] and sums[1] == sums[2]:
             return 1
 
     return 0
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *A = list(map(int, input.split()))
-    print(partition3(A))
-
+blah = [17,59,34,57,17,23,67,1,18,2,59]
+print(partition3(blah))
